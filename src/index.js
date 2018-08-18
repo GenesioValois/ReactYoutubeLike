cdb27db73e0cd223824b28acd2d1c404e71bdf7c
Genésio Valois <genas96@gmail.com>
2018-08-18 14:46:21 -0300

    fetching search bar component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import {API_KEY} from './credentials'
 
+import SearchBar from './components/search_bar'
+
 // Create new component
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Put this component on the DOM
