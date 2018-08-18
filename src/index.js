7eb39b3dd6ad704e6abbbcb56cf13b5af3db31b3
Genésio Valois <genas96@gmail.com>
2018-08-18 14:12:09 -0300

    first component

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+// Create new component
+const App = () => {
+  return <div>Hi</div>;
+}
+
+// Put this component on the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
+
